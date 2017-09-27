2bc147abbec6ad26dc242f6e1daf83c4cad2e138
racalhoun <racalhoun489@gmail.com>
2017-09-27 10:50:36 -0400

    set mongo connect and variables

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+const db = mongoose.connection
+db.on('error', (error)=>{
+  console.log(error)
+})
+db.once('open', ()=>{
+  console.log('connected to Mongodb')
+})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
